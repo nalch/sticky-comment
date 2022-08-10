2c9aa8a0d7e67c61e12c6251d2cf84407dd468ca
nalch <scholze.kristian@gmail.com>
2022-08-10 15:34:39 +0100

    v6

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,3 +2,12 @@
 
 echo "Hello $1 $2"
 printenv
+
+#GITHUB_REF=refs/pull/1/merge
+#RUNNER_WORKSPACE=/home/runner/work/sticky-comment
+#GITHUB_PATH=/github/file_commands/add_path_98d9aa35-fed0-491e-9dfa-dc6ca346b37e
+#GITHUB_SERVER_URL=https://github.com
+#GITHUB_EVENT_NAME=pull_request
+
+gh
+
